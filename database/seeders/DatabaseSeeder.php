405d9fa69e6893b5531eadaa039d07886081434a
natdm02 <natalidimarino2002@gmail.com>
2024-05-29 21:43:23 +0200

    [add] tableseeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,8 +19,9 @@ class DatabaseSeeder extends Seeder
         //     'email' => 'test@example.com',
         // ]);
         $this->call([
+            TechnologiesTableSeeder::class,
             TypesTableSeeder::class,
-            //ProjectsTableSeeder::class
+            ProjectsTableSeeder::class
         ]);
     }
 }
