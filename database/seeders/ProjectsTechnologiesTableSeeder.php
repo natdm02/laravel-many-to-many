09358d2a625b415b2b4116423e26c298131d3bb0
natdm02 <natalidimarino2002@gmail.com>
2024-05-29 23:53:51 +0200

    [add] insertions

--- database/seeders/ProjectsTechnologiesTableSeeder.php
+++ database/seeders/ProjectsTechnologiesTableSeeder.php
@@ -4,6 +4,9 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Project;
+use App\Models\Technology;
+use Illuminate\Support\Facades\DB;
 
 class ProjectsTechnologiesTableSeeder extends Seeder
 {
@@ -12,6 +15,22 @@ class ProjectsTechnologiesTableSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        for($i = 0; $i < 50; $i++){
+
+            $project = Project::inRandomOrder()->first();
+
+            $technology_id = Technology::inRandomOrder()->first()->id;
+
+
+            $check_project = DB::table('project_technology')
+                ->where('project_id', $project->id)
+                ->where('technology_id', $technology_id)
+                ->count();
+
+            if (!$check_project) {
+                $project->technologies()->attach($technology_id);
+            }
+
+        }
     }
 }
