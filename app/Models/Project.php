5252a52514040f6ee09f1d73f9cab5aa3f964ff6
natdm02 <natalidimarino2002@gmail.com>
2024-05-29 23:44:50 +0200

    [add]adding methods to models

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -36,4 +36,7 @@ class Project extends Model
     public function type() {
         return $this->belongsTo(Type::class);
     }
+    public function technologies() {
+        return $this->belongsToMany(Technology::class);
+    }
 }
