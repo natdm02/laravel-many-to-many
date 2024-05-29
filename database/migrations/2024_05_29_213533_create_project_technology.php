6edcbca6a1b2791854c1379376de7eba96b5156f
natdm02 <natalidimarino2002@gmail.com>
2024-05-29 23:40:58 +0200

    [add] table project

--- database/migrations/2024_05_29_213533_create_project_technology.php
+++ database/migrations/2024_05_29_213533_create_project_technology.php
@@ -12,8 +12,20 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('project_technology', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
+
+            $table->unsignedBigInteger('project_id');
+
+            $table  ->foreign('project_id')
+                    ->references('id')
+                    ->on('projects')
+                    ->cascadeOnDelete();
+
+            $table->unsignedBigInteger('technology_id');
+
+            $table  ->foreign('technology_id')
+                    ->references('id')
+                    ->on('technologies')
+                    ->cascadeOnDelete();
         });
     }
 
